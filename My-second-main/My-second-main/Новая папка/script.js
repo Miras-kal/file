f8af91a2b7f010479963b5c0d40e7d78eaa82791
Miras-kal <130293467+Miras-kal@users.noreply.github.com>
2023-11-04 14:50:21 +0600

    Add files via upload

---
+++ My-second-main/My-second-main/Новая папка/script.js
@@ -0,0 +1,42 @@
+let position = 0;
+const sliderLine = document.querySelector('.slider_line')
+
+const right = document.querySelector('.slider_right')
+
+let oposite = 1
+
+right.addEventListener('click', function style(){
+    position = position + 1200;
+    if(position ===3600)position = 0
+    sliderLine.style.right = position + 'px'    
+    oposite = oposite + 1
+    if(oposite === 4 || oposite <= 0)oposite = 1
+    console.log(oposite)
+    return oposite
+})
+const left = document.querySelector('.slider_left')
+
+left.addEventListener('click', function style(){
+        position = position - 1200
+        if(position < 0)position = 2400
+        sliderLine.style.right = position + 'px'
+        oposite = oposite - 1
+        if(oposite <= 0)oposite = 3
+        console.log(oposite)
+        return oposite
+})
+
+
+const SliderUnder = document.querySelector('.slider_under_div')
+
+if(oposite ===2){
+    SliderUnder.style.backgroundColor = '№ссс'
+        console.log('вы коньченные ублютки')
+}
+
+// function sliderUnderfunc(oposite){
+//     if(oposite > 1){
+//         SliderUnder.style.backgroundColor = 'black'
+//         console.log(oposite)
+//     }
+// }
